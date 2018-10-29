99799dfe74dc5695a45cb2efb68e629f63d16914
ZqhKHP <G00336837@gmit.ie>
2018-10-29 21:09:24 +0000

    adding the booking service to the rmi registry

--- dsRMI_DataAccess/src/main/java/ie/gmit/sw/ds/dsRMI_DataAccess/App.java
+++ dsRMI_DataAccess/src/main/java/ie/gmit/sw/ds/dsRMI_DataAccess/App.java
@@ -1,13 +1,25 @@
 package ie.gmit.sw.ds.dsRMI_DataAccess;
 
-/**
- * Hello world!
- *
- */
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main(String[] args) throws Exception {
+		IBookingService bookingService = new BookingServiceImpl();
+
+		LocateRegistry.createRegistry(1099); // Start the RMI registry on port
+												// 1099
+
+		Naming.rebind("bookingService", bookingService); // Bind our remote
+															// object to the
+															// registry with the
+															// human-readable
+															// name
+															// "bookingService"
+
+		System.out.println("Server ready."); // Print a message to standard
+												// output
+	}
 }
