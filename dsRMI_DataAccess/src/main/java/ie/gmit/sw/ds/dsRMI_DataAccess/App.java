69c68a93442ae1876d88995e8d662bd3ea666667
ZqhKHP <G00336837@gmit.ie>
2018-10-29 18:39:49 +0000

    adding initial project files for the RMI data access layer

---
+++ dsRMI_DataAccess/src/main/java/ie/gmit/sw/ds/dsRMI_DataAccess/App.java
@@ -0,0 +1,13 @@
+package ie.gmit.sw.ds.dsRMI_DataAccess;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
