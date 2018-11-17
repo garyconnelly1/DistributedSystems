76213ffff3519f6eba8d2ae061cc8b8c94d79709
ZqhKHP <G00336837@gmit.ie>
2018-11-17 15:20:15 +0000

    adding an unmarshalling method to the BookingMarshal superclass

--- dsRESTfulServiceProject/src/main/java/ie/gmit/sw/dsMarshal/BookingMarshal.java
+++ dsRESTfulServiceProject/src/main/java/ie/gmit/sw/dsMarshal/BookingMarshal.java
@@ -1,12 +1,16 @@
 package ie.gmit.sw.dsMarshal;
 
+import java.io.StringReader;
 import java.io.StringWriter;
 
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
 
 import ie.gmit.sw.dsModels.Booking;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 //import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 //import javax.xml.bind.Unmarshaller;
@@ -31,6 +35,26 @@ public class BookingMarshal {
 		}
 		
 		return sw.toString();
+	}// end getBookingAsXML
+	
+	private Booking getBookingFromXML(String input) {
+		// Unmarshal the Booking from XML
+		StringReader sr1 = new StringReader(input);
+		Unmarshaller um1;
+		Booking bookingFromXml = null;
+		
+		try {
+			JAXBContext jc = JAXBContext.newInstance("ie.gmit.sw.dsModels");
+			um1 = jc.createUnmarshaller();
+			StreamSource source1 = new StreamSource(sr1);
+			JAXBElement<Booking> bookingElement1 = um1.unmarshal(source1, Booking.class);
+			bookingFromXml = (Booking) bookingElement1.getValue();
+		} catch (JAXBException e) {
+			System.out.println("Problem unmarshalling booking object");
+			e.printStackTrace();
+		}
+		
+		return bookingFromXml;
 	}
 
 }
