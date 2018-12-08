1f1d4add4b6eed05c0a6df6e296b6f17b2db2549
ZqhKHP <G00336837@gmit.ie>
2018-12-08 17:21:51 +0000

    POST working from the client console

--- dsClient/src/main/java/ie/gmit/sw/dsConsumer/Consumer.java
+++ dsClient/src/main/java/ie/gmit/sw/dsConsumer/Consumer.java
@@ -5,18 +5,24 @@ import java.util.List;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import ie.gmit.sw.dsMarshal.BookingMarshal;
 import ie.gmit.sw.dsModels.Booking;
 import ie.gmit.sw.dsModels.Bookings;
+import ie.gmit.sw.dsModels.Customer;
+import ie.gmit.sw.dsModels.Vehicle;
 
 
 
 public class Consumer extends BookingMarshal{
 
 	public static void main(String[] args) {
+		
+		String base = "http://localhost:8080/dsRESTfulServiceProject/webapi/bookings";
 		
 		System.out.println("Consumer");
 		
@@ -26,7 +32,7 @@ public class Consumer extends BookingMarshal{
 		System.out.println("Loading ... ");
 		
 		// 2. Set a target for the client.
-		WebTarget target = client.target("http://localhost:8080/dsRESTfulServiceProject/webapi/bookings");
+		WebTarget target = client.target(base);
 		
 		
 		
@@ -50,11 +56,50 @@ public class Consumer extends BookingMarshal{
 		}
 		
 		System.out.println("bookings");
-	//	System.out.println(booking.getBookingId());
 		
-	//	System.out.println(
-		//		target.request(MediaType.APPLICATION_XML).get(String.class)
-		//		);
+		////////////////////////////////////////////
+		// Trying to do a POST with a booking object
+		target = client.target(base + "/1");
+		xmlResponse = target.request(MediaType.APPLICATION_XML).get(String.class);
+		Booking newBooking = getBookingFromXML(xmlResponse);
+		System.out.println("SINGLE BOOKING UNMARSHALLED");
+		
+		System.out.println("XML RESPONSE 1 BOOKING");
+		System.out.println(xmlResponse);
+		
+		System.out.println(newBooking.getBookingId());
+		
+		Customer customer = new Customer();
+		Vehicle vehicle = new Vehicle();
+		Booking booking = new Booking();
+		
+		customer.setCustomerId(10);
+		vehicle.setId(10);
+		booking.setBookingId(10);
+		booking.setCustomer(customer);
+		booking.setVehicle(vehicle);
+		booking.setStartDate("03-03-2019");
+		booking.setEndDate("04-03-2019");
+		booking.setBookingNumber(null);
+		
+		
+		
+		target = client.target(base);
+		
+		String send = getBookingAsXML(booking);
+		
+		System.out.println(send);
+		
+		//System.out.println(send);
+		
+		Response response = target.request().post(Entity.xml(send));
+		
+		System.out.println("=====================================================");
+		System.out.println(response);
+		System.out.println("=====================================================");
+		
+		
+		
 
 	}
 
