915720a487be8c7897252fef148094d117e3a83a
ZqhKHP <G00336837@gmit.ie>
2018-12-10 11:06:16 +0000

    Create Booking working on the Desktop client

--- dsDesktopClient/src/main/java/ie/gmit/sw/dsConsumer/DesktopMain.java
+++ dsDesktopClient/src/main/java/ie/gmit/sw/dsConsumer/DesktopMain.java
@@ -24,29 +24,27 @@ public class DesktopMain {
 		boolean isContinue = true;
 		Scanner reader = new Scanner(System.in); // Reading from System.in
 
-		
-
 		while (isContinue) {
 
 			int id;
-			
 
 			System.out.println("Welcome to Distributed Systems Projecr(Desktop version)");
 			System.out.println("Press 1: ----------------------> Read all Bookings.");
 			System.out.println("Press 2: ----------------------> Update a Booking.");
+			System.out.println("Press 3: ----------------------> Create a new Booking.");
+
 			String s = reader.nextLine();
 			if (s.equals("1")) {
 				System.out.println("You pressed: " + s);
 				System.out.println("Loading(Please wait a moment...) ");
 				getAllBookings();
-			}
-			else if(s.equals("2")) {
-				
+			} else if (s.equals("2")) {
+
 				System.out.println("Enter the ID of the booking you wish to update.");
 				id = reader.nextInt();
 				updateBooking(id);
-				
-				
+			} else if (s.equals("3")) {
+				createBooking();
 			}
 
 			System.out.println("Type 'QUIT' to exit Program, Any other key to continue:");
@@ -64,8 +62,8 @@ public class DesktopMain {
 	public static void getAllBookings() {
 		RestAccess access = new RestAccess();
 		List<Booking> bookingList = access.getBookings();
-		
-		for(Booking b: bookingList) {
+
+		for (Booking b : bookingList) {
 			System.out.println("===============================");
 			System.out.println("Booking ID: \t" + b.getBookingId());
 			System.out.println("Customer ID: \t" + b.getCustomer().getCustomerId());
@@ -75,16 +73,16 @@ public class DesktopMain {
 			System.out.println("===============================");
 			System.out.println("");
 		}
-		
+
 	}
-	
+
 	public static void updateBooking(int id) {
 		Scanner reader = new Scanner(System.in); // Reading from System.in
-		
+
 		RestAccess access = new RestAccess();
-		
+
 		Booking booking = access.getBookingById(id);
-		
+
 		System.out.println("===============================");
 		System.out.println("Booking ID: \t" + booking.getBookingId());
 		System.out.println("Customer ID: \t" + booking.getCustomer().getCustomerId());
@@ -93,42 +91,78 @@ public class DesktopMain {
 		System.out.println("End Date: \t" + booking.getEndDate());
 		System.out.println("===============================");
 		System.out.println("");
-		
+
 		System.out.println("Press 1: ----------> To update Customer_ID");
 		System.out.println("Press 2: ----------> To update Vehicle_ID");
 		System.out.println("Press 3: ----------> To update Start_Date");
 		System.out.println("Press 4: ----------> To update End_Date");
 		String input = reader.nextLine();
-		if(input.equals("1")) {
+		if (input.equals("1")) {
 			System.out.println("Enter the new Customer_ID");
 			int newId = reader.nextInt();
 			Customer customer = new Customer();
 			customer.setCustomerId(newId);
 			booking.setCustomer(customer);
-		}
-		else if(input.equals("2")) {
+		} else if (input.equals("2")) {
 			System.out.println("Enter the new Customer_ID");
 			int newId = reader.nextInt();
 			Vehicle vehicle = new Vehicle();
 			vehicle.setId(newId);
 			booking.setVehicle(vehicle);
-		}
-		else if(input.equals("3")) {
+		} else if (input.equals("3")) {
 			System.out.println("Enter the new Start_Date");
 			String newDate = reader.nextLine();
 			booking.setStartDate(newDate);
-		}
-		else if(input.equals("d")) {
+		} else if (input.equals("d")) {
 			System.out.println("Enter the new End_Date");
 			String newDate = reader.nextLine();
 			booking.setEndDate(newDate);
-		}
-		else {
+		} else {
 			System.out.println("Not one of the options.");
 		}
-		
+
 		access.updateBooking(booking);
 		System.out.println("Booking updated!");
+	}
+
+	public static void createBooking() {
+
+		Scanner reader = new Scanner(System.in); // Reading from System.in
+
+		RestAccess access = new RestAccess();
+		
+		System.out.println("===============================");
+		System.out.println("Enter the Booking ID: ");
+		int bookingId = reader.nextInt();
+		System.out.println("Enter the Customer ID: ");
+		int customerId = reader.nextInt();
+		System.out.println("Enter the vehicle ID: ");
+		int vehicleId = reader.nextInt();
+		System.out.println("Enter the Start Date: ");
+		String startDate = reader.next();
+		System.out.println("End the End Date: ");
+		String endDate = reader.next();
+		
+		Customer customer = new Customer();
+		customer.setCustomerId(customerId);
+		Vehicle vehicle = new Vehicle();
+		vehicle.setId(vehicleId);
+		Booking booking = new Booking();
+		
+		booking.setBookingId(bookingId);
+		booking.setCustomer(customer);
+		booking.setVehicle(vehicle);
+		booking.setStartDate(startDate);
+		booking.setEndDate(endDate);
+		
+		System.out.println("===============================");
+		System.out.println("Creating Booking...");
+		
+		access.createBooking(booking);
+		
+		System.out.println("Booking Created!");
+		
+		
 		
 	}
 
